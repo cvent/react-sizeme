4568547f0168a4c30ada7088402395864d25c54f
Li, Li <chrisy.lili@gmail.com>
2018-02-20 10:29:49 -0500

    Allow two strategies for resize detector

--- commonjs/resizeDetector.js
+++ commonjs/resizeDetector.js
@@ -10,20 +10,29 @@ var _elementResizeDetector2 = _interopRequireDefault(_elementResizeDetector);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var instance = void 0;
+var scrollInstance = void 0;
+var objectInstance = void 0;
 
 // Lazily require to not cause bug
 // https://github.com/ctrlplusb/react-sizeme/issues/6
 function resizeDetector() {
   var strategy = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'scroll';
 
-  if (!instance) {
-    instance = (0, _elementResizeDetector2.default)({
+  if (strategy === 'scroll') {
+    if (scrollInstance) return scrollInstance;
+    scrollInstance = (0, _elementResizeDetector2.default)({
       strategy: strategy
     });
+    return scrollInstance;
   }
 
-  return instance;
+  if (strategy === 'object') {
+    if (objectInstance) return objectInstance;
+    objectInstance = (0, _elementResizeDetector2.default)({
+      strategy: strategy
+    });
+    return objectInstance;
+  }
 }
 
 exports.default = resizeDetector;
--- src/resizeDetector.js
+++ src/resizeDetector.js
@@ -1,17 +1,26 @@
 import createResizeDetector from 'element-resize-detector'
 
-let instance
+let scrollInstance
+let objectInstance
 
 // Lazily require to not cause bug
 // https://github.com/ctrlplusb/react-sizeme/issues/6
 function resizeDetector(strategy = 'scroll') {
-  if (!instance) {
-    instance = createResizeDetector({
+  if (strategy === 'scroll') {
+    if (scrollInstance) return scrollInstance
+    scrollInstance = createResizeDetector({
       strategy,
     })
+    return scrollInstance
   }
 
-  return instance
+  if (strategy === 'object') {
+    if (objectInstance) return objectInstance
+    objectInstance = createResizeDetector({
+      strategy,
+    })
+    return objectInstance
+  }
 }
 
 export default resizeDetector
